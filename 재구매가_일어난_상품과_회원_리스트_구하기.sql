a349fca8253520fcb61615e04dc49b2fe838031e
June Young Jung <jjy1111202@naver.com>
2025-09-30 16:52:47 +0900

    Create 재구매가_일어난_상품과_회원_리스트_구하기.sql

---
+++ 재구매가_일어난_상품과_회원_리스트_구하기.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+SELECT 
+  o.USER_ID,
+  o.PRODUCT_ID
+ from ONLINE_SALE o
+ GROUP BY o.USER_ID, o.PRODUCT_ID 
+ HAVING count(*) >= 2
+ -- WHERE 절에서는 COUNT(*) 같은 집계함수를 바로 쓸 수 없음
+ ORDER BY o.USER_ID, o.PRODUCT_ID desc;
