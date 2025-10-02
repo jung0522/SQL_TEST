84b4a8b6c42fe4ea43920ac63a94fa2f4bc82846
June Young Jung <jjy1111202@naver.com>
2025-10-02 14:00:12 +0900

    Create 조건에_맞는_개발자_찾기.sql

---
+++ 조건에_맞는_개발자_찾기.sql
@@ -0,0 +1,8 @@
+-- DISTINCT 없이는 한 개발자가 여러 스킬 때문에 중복으로 나오므로 채점에서 실패
+SELECT DISTINCT d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME
+FROM DEVELOPERS d
+JOIN SKILLCODES s
+-- 이진수 & 연산
+ON (d.SKILL_CODE & s.CODE) != 0
+WHERE s.NAME IN ('Python' , 'C#')
+ORDER BY d.ID
