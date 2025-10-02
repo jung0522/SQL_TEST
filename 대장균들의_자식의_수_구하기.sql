485ddada14e75d707b4ce989e8178b8a7fe0e0a2
June Young Jung <jjy1111202@naver.com>
2025-10-02 15:40:25 +0900

    Update 대장균들의_자식의_수_구하기.sql

--- 대장균들의_자식의_수_구하기.sql
+++ 대장균들의_자식의_수_구하기.sql
@@ -1,6 +1,7 @@
 -- 부모 자식이면 p c로 자체 조인
 SELECT p.ID, COUNT(c.id) AS CHILD_COUNT
 FROM ECOLI_DATA p
+-- LEFT 이유: 자식이 없는 경우도 CHILD_COUNT = 0으로 출력해야 하기 때문입니다.
 LEFT JOIN ECOLI_DATA c
 ON p.id = c.PARENT_ID
 GROUP BY p.ID
