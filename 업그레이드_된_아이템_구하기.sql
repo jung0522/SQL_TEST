f63694028855842ee506977d09ce86d04fd17655
June Young Jung <jjy1111202@naver.com>
2025-10-02 15:36:02 +0900

    트리의 부모 아이디 = 부모의 아이디

--- 업그레이드_된_아이템_구하기.sql
+++ 업그레이드_된_아이템_구하기.sql
@@ -1,12 +1,14 @@
--- 부모 자식 관계면 join 2번
+-- 부모 자식 관계에 테이블 2개면 join 2번
 SELECT 
 c.ITEM_ID, 
 c.ITEM_NAME,
 c.RARITY
 FROM ITEM_TREE t
 JOIN ITEM_INFO p 
+-- 트리의 부모 아이디 = 부모의 아이디
 ON t.PARENT_ITEM_ID = p.ITEM_ID
 JOIN ITEM_INFO c
+-- 트리의 아이디 = 자식의 아이디
 ON t.ITEM_ID = c.ITEM_ID
 WHERE p.RARITY = 'RARE'
 ORDER BY c.ITEM_ID DESC;
